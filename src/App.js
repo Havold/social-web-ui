6549615206cdbcba68c6dfd469521ce97777ef22
Havold <21521479@gm.uit.edu.vn>
2024-09-09 23:03:49 +0700

    Create basic Layout and use Protected Route (Auth).

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import LeftBar from "./components/LeftBar/LeftBar";
+import NavBar from "./components/NavBar/NavBar";
+import RightBar from "./components/RightBar/RightBar";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
+  const currentUser = false;
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />
+    }
+
+    return children;
+  }
+
+  const Layout = () => {
+    return (
+      <div className="layout">
+        <NavBar />
+        <div style={{display: 'flex'}}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
   const router = createBrowserRouter([
     {
       path: "/login",
@@ -12,6 +40,19 @@ function App() {
       path: "/register",
       element: <Register />,
     },
+    {
+      path: '/',
+      element: <ProtectedRoute><Layout /></ProtectedRoute>,
+      children: [{
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/profile/:id',
+        element: <Profile />
+      }
+    ]
+    }
   ]);
   return <div className="App">
     <RouterProvider router={router}/>
