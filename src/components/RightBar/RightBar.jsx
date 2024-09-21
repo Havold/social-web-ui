cd4cca92089dc23f52fad41e36689b8c7c4199de
Havold <21521479@gm.uit.edu.vn>
2024-09-22 00:07:52 +0700

    Create Share your thought UI.

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -5,7 +5,7 @@ import UserInfo from '../UserInfo/UserInfo';
 
 const RightBar = () => {
     const avatar =
-        'https://p9-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/25cc895137a04da3482b762a567a9046.jpeg?lk3s=a5d48078&nonce=51733&refresh_token=1ad755b6678028489c2128ee89e7bd4e&x-expires=1726660800&x-signature=eaCxU%2FqBdZ5f%2BtO0euZoxtKzKBY%3D&shp=a5d48078&shcp=81f88b70';
+        'https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/25cc895137a04da3482b762a567a9046.jpeg?lk3s=a5d48078&nonce=91568&refresh_token=f0b665e59cdd1a4c66c611c4a4d71fd2&x-expires=1727103600&x-signature=vY7oO3Kozd4WJHhn4fku%2BR4tt9M%3D&shp=a5d48078&shcp=81f88b70';
     return (
         <div className="rightBar">
             <div className="container">
---
+++ src/components/Share/Share.jsx
@@ -0,0 +1,46 @@
+import { useContext } from 'react';
+import './share.scss';
+import { AuthContext } from '../../context/authContext';
+import { InsertPhotoRounded, AddLocationRounded, LoyaltyRounded } from '@mui/icons-material';
+
+const Share = () => {
+    const { currentUser } = useContext(AuthContext);
+    return (
+        <div className="share">
+            <div className="top">
+                <img src={currentUser.profilePic} alt="avatar" />
+                <textarea placeholder={`What's on your mind ${currentUser.name}?`} />
+            </div>
+            <div className="bottom">
+                <div className="left">
+                    <div className="item">
+                        <input type="file" id="uploadFile" />
+                        <label htmlFor="uploadFile">
+                            <InsertPhotoRounded className="icon" />
+                            <span>Add Image</span>
+                        </label>
+                    </div>
+                    <div className="item">
+                        <input type="file" />
+                        <label htmlFor="">
+                            <AddLocationRounded className="icon" />
+                            <span>Add Place</span>
+                        </label>
+                    </div>
+                    <div className="item">
+                        <input type="file" />
+                        <label htmlFor="">
+                            <LoyaltyRounded className="icon" />
+                            <span>Tag Friends</span>
+                        </label>
+                    </div>
+                </div>
+                <div className="right">
+                    <button>Share</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Share;
