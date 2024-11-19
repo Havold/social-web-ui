1df0172fa7081bd5bbb9717d9d2fb0e473840011
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-11-19 18:04:42 +0700

    Add images in public/images

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -4,8 +4,7 @@ import UserActivity from '../UserActivity/UserActivity';
 import UserInfo from '../UserInfo/UserInfo';
 
 const RightBar = () => {
-    const avatar =
-        'https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/25cc895137a04da3482b762a567a9046.jpeg?lk3s=a5d48078&nonce=91568&refresh_token=f0b665e59cdd1a4c66c611c4a4d71fd2&x-expires=1727103600&x-signature=vY7oO3Kozd4WJHhn4fku%2BR4tt9M%3D&shp=a5d48078&shcp=81f88b70';
+    const avatar = '/upload/1727962858790haitu.jpg';
     return (
         <div className="rightBar">
             <div className="container">
--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -4,24 +4,26 @@ import { AuthContext } from '../../context/authContext';
 
 function Stories() {
     const { currentUser } = useContext(AuthContext);
+    const avatar = '/upload/1727962858790haitu.jpg';
+    const img = '/upload/1727964002522japan.jpg';
     const stories = [
         {
             id: '1',
-            name: 'Havold',
-            avatar: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149128164_1379855842365987_6424596517571655275_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeHezHCbi3oaiQjzSf8pFNC6kGGyVcIOkTuQYbJVwg6ROzjFWJLz-Q3Y7dJO3NqfSjxPAbm_YwXdKnJMi6hIg8dK&_nc_ohc=c9BgkSROktMQ7kNvgF8SuVn&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=AdTHq8VtDCZ3JmH_oIe44yI&oh=00_AYCItTCuBLW1EugSkUneaU4ICLI7xMdDgd66LDcRjimy0Q&oe=670FBB5D',
-            img: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149657892_1379856125699292_4884889491938171786_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=2a1932&_nc_eui2=AeGU7RJaEp0Z0Rl3KyQ60xfQTPV7dT68BzpM9Xt1PrwHOm23mWms6PEHJfEx31kjsX-Hdr_p8oDmZjdDqmmvHlI8&_nc_ohc=gEJN-u3VcMoQ7kNvgHu2RwU&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=ALBPid4Q6JOjAaBvtUb0q6J&oh=00_AYCGWI7qymD2b9LHgjW3V9Sj19J8G48LJGxZXNbvmWSI-g&oe=670FC69F',
+            name: 'Hai Tu',
+            avatar: avatar,
+            img: img,
         },
         {
             id: '2',
-            name: 'Havold',
-            avatar: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149128164_1379855842365987_6424596517571655275_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeHezHCbi3oaiQjzSf8pFNC6kGGyVcIOkTuQYbJVwg6ROzjFWJLz-Q3Y7dJO3NqfSjxPAbm_YwXdKnJMi6hIg8dK&_nc_ohc=c9BgkSROktMQ7kNvgF8SuVn&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=AdTHq8VtDCZ3JmH_oIe44yI&oh=00_AYCItTCuBLW1EugSkUneaU4ICLI7xMdDgd66LDcRjimy0Q&oe=670FBB5D',
-            img: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149657892_1379856125699292_4884889491938171786_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=2a1932&_nc_eui2=AeGU7RJaEp0Z0Rl3KyQ60xfQTPV7dT68BzpM9Xt1PrwHOm23mWms6PEHJfEx31kjsX-Hdr_p8oDmZjdDqmmvHlI8&_nc_ohc=gEJN-u3VcMoQ7kNvgHu2RwU&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=ALBPid4Q6JOjAaBvtUb0q6J&oh=00_AYCGWI7qymD2b9LHgjW3V9Sj19J8G48LJGxZXNbvmWSI-g&oe=670FC69F',
+            name: 'Hai Tu',
+            avatar: avatar,
+            img: img,
         },
         {
             id: '3',
-            name: 'Havold',
-            avatar: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149128164_1379855842365987_6424596517571655275_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeHezHCbi3oaiQjzSf8pFNC6kGGyVcIOkTuQYbJVwg6ROzjFWJLz-Q3Y7dJO3NqfSjxPAbm_YwXdKnJMi6hIg8dK&_nc_ohc=c9BgkSROktMQ7kNvgF8SuVn&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=AdTHq8VtDCZ3JmH_oIe44yI&oh=00_AYCItTCuBLW1EugSkUneaU4ICLI7xMdDgd66LDcRjimy0Q&oe=670FBB5D',
-            img: 'https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/149657892_1379856125699292_4884889491938171786_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=2a1932&_nc_eui2=AeGU7RJaEp0Z0Rl3KyQ60xfQTPV7dT68BzpM9Xt1PrwHOm23mWms6PEHJfEx31kjsX-Hdr_p8oDmZjdDqmmvHlI8&_nc_ohc=gEJN-u3VcMoQ7kNvgHu2RwU&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=ALBPid4Q6JOjAaBvtUb0q6J&oh=00_AYCGWI7qymD2b9LHgjW3V9Sj19J8G48LJGxZXNbvmWSI-g&oe=670FC69F',
+            name: 'Hai Tu',
+            avatar: avatar,
+            img: img,
         },
     ];
     return (
